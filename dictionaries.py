d752cc71fb393454383912d71dbfaae5a513f5cc
Chris Harvey <chris.harvey0248@yahoo.com>
2018-06-12 10:59:56 -0500

    Created Dictionaries.py

---
+++ dictionaries.py
@@ -0,0 +1,25 @@
+my_dict = { 'name': 'Chris', 'age': 22}
+fib_dict = { 1: 8, 2: 13, 3: 21}
+lottery_player = {
+  'name': 'John',
+  'numbers':(13, 45, 66, 23, 22)
+}
+
+universitites = [
+  {
+    'name': 'Oxford',
+    'location': 'UK'
+  },
+  {
+    'name': 'MIT',
+    'location': 'US'
+  }
+]
+
+dict_in_dict = {
+  'key': {
+    'name': 'Chris'
+  }
+}
+
+lottery_sum = sum(lottery_player['numbers'])
