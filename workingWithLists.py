96e6d7a8254c116e9c27db9e5db47a3e2d9c27ed
Chris Harvey <chris.harvey0248@yahoo.com>
2018-06-12 10:01:43 -0500

    Created Files

---
+++ workingWithLists.py
@@ -0,0 +1,27 @@
+# known_people = ['Katelyn', 'Nicole', 'Harvey']
+# person = input('Enter the person you know:')
+
+# if person in known_people:
+#   print('You know {}!'.format(person))
+# else:
+#   print("You don't know {}!".format(person))
+
+# Exercise
+def who_do_you_know():
+  people = input('Enter the name of people you know, separated by commas: ')
+  people_list = people.split(",")
+
+  people_without_spaces = []
+  for person in people_list:
+    people_without_spaces.append(person.strip())
+    
+  return people_without_spaces
+
+def ask_user():
+  person = input('Enter a name of someone you know: ')
+  if person in who_do_you_know():
+    print('You know {}!'.format(person))
+  else:
+    print("You don't know {}!".format(person))
+
+ask_user()
