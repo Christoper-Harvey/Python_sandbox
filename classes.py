1935ea87eb2be718932fb0874c52058520d11eb4
Chris Harvey <chris.harvey0248@yahoo.com>
2018-06-12 13:31:11 -0500

    added examples to classes.py

--- classes.py
+++ classes.py
@@ -9,6 +9,23 @@ class LotteryPlayer:
     self.numbers = (5, 9, 12, 3, 1, 21)
 
 player_one = LotteryPlayer('Chris')
+player_one.numbers = (1, 2, 4, 21, 48, 5)
 player_two = LotteryPlayer('Katelyn')
 
 print(player_one.name == player_two.name)
+
+class Student:
+  def __init__(self, name, school):
+    self.name = name
+    self.school = school
+    self.marks = []
+
+  def average(self):
+    return sum(self.marks) / len(self.marks)
+
+anna = Student('Anna', 'MIT')
+anna.marks.append(56)
+anna.marks.append(72)
+anna.marks.append(98)
+print(anna.marks)
+print(anna.average())
